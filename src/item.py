ccd477eef34ca4f454a5d29147e4d96f30009c06
Matthew Kuczak <matthewkuczak@gmail.com>
2018-08-30 10:55:39 -0500

    Added item.py. Also fixed error where extra empty string input parameter was present in both test Items, and changed assert() format to self.assertEqual() for more effective troubleshooting (this will show the differences between expected and actual side-by-side rather than requiring me to integrate print statements).

---
+++ src/item.py
@@ -0,0 +1,27 @@
+class Item:
+
+    def __init__(self, manufacturer, product, price, multiplier, quantity, barcode=None):
+        if barcode != None:
+            self.barcode = barcode
+        else:
+            self.barcode = ""
+        self.manufacturer = manufacturer
+        self.product = product
+        self.price = price
+        self.multiplier = multiplier
+        self.quantity = quantity
+
+    def build_barcode(self, first, manufacturer_code, product_code):
+        b = [int(first)]
+        for n in manufacturer_code:
+            b.append(int(n))
+        for n in product_code:
+            b.append(int(n))
+        # Below is the logic for the check digit.  Read more about it here:
+        # http://www.azalea.com/white-papers/upc-barcode-check-digit/
+        check_digit = str((10 - (3*(b[0]+b[2]+b[4]+b[6]+b[8]+b[10])+(b[1]+b[3]+b[5]+b[7]+b[9]))) % 10)
+        barcode = ""
+        for n in b:
+            barcode += str(n)
+        barcode += check_digit
+        self.barcode = barcode
--- test/test_item.py
+++ test/test_item.py
@@ -7,14 +7,14 @@ from src.item import Item
 class TestTransaction(unittest.TestCase):
 
     def setUp(self):
-        self.e = Item('', "General Mills", "Honey Nut Cheerios Cereal - 12oz", 3.99, 1.00, 90)  # 016000275270
-        self.f = Item('', "Smucker's", "Smucker's: Cherry Preserves - 18oz", 4.55, 0.85, 0)  # 051500006771
+        self.e = Item("General Mills", "Honey Nut Cheerios Cereal - 12oz", 3.99, 1.00, 90)  # 016000275270
+        self.f = Item("Smucker's", "Smucker's: Cherry Preserves - 18oz", 4.55, 0.85, 0)  # 051500006771
 
     def test_build_barcode(self):
         self.e.build_barcode('0', '16000', '27527')
-        assert(self.e.barcode == '016000275270')
+        self.assertEqual(self.e.barcode, '016000275270')
         self.f.build_barcode('0', '51500', '00677')
-        assert(self.f.barcode == '051500006771')
+        self.assertEqual(self.f.barcode, '051500006771')
 
 
 # Code below lets me test the code by running this py file.

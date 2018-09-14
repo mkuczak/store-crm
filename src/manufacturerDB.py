c2890a66e56116542ebae56889d859a2bdfd9064
Matthew Kuczak <matthewkuczak@gmail.com>
2018-09-13 19:00:22 -0500

    Completed manufacturerDB.py and deleted test_manufacturerDB.py.  The tests were too confusing, which I felt was ruining the point of the tests.  I'll just manually be viewing the DB file using a reader to test.

--- src/manufacturerDB.py
+++ src/manufacturerDB.py
@@ -3,35 +3,57 @@ from src.manufacturer import*
 
 
 def add_to_db(manufacturer):
-    pass
+    _rules = []
+    for n in range(0, 5):
+        try:
+            _rules.append(manufacturer.rules[n])
+        except IndexError:
+            _rules.append("")
+    cursor.execute("INSERT INTO Manufacturers VALUES (:Manufacturer, :Code, :Rule_1, :Rule_2, :Rule_3, :Rule_4,"
+                   ":Rule_5)",
+                   {'Manufacturer': manufacturer.name, 'Code': manufacturer.code,
+                    'Rule_1': _rules[0], 'Rule_2': _rules[1], 'Rule_3': _rules[2], 'Rule_4': _rules[3],
+                    'Rule_5': _rules[4]})
+    connector.commit()
 
 
 def extract_from_db_by_name(name):
-    pass
+    row = cursor.execute("SELECT * FROM Manufacturers WHERE Name='?'", name).fetchone()
+    return Manufacturer(row[0], row[1], [row[2], row[3], row[4], row[5], row[6], row[7]])
 
 
 def extract_from_db_by_code(code):
+    row = cursor.execute("SELECT * FROM Manufacturers WHERE Code='?'", code).fetchone()
+    return Manufacturer(row[0], row[1], [row[2], row[3], row[4], row[5], row[6], row[7]])
 
 
 def remove_from_db_by_name(name):
-    pass
+    cursor.execute("DELETE * FROM Manufacturers WHERE Name='?'", name)
 
 
 def remove_from_db_by_code(code):
-    pass
+    cursor.execute("DELETE * FROM Manufacturers WHERE Code='?'", code)
 
 
 def get_name_from_code(code):
-    pass
+    return cursor.execute("SELECT Name FROM Manufacturers WHERE Code='?'", code).fetchone()[0]
 
 
 def get_code_from_name(name):
-    pass
+    return cursor.execute("SELECT Code FROM Manufacturers WHERE Name='?'", name).fetchone()[1]
 
 
 def get_rules_from_code(code):
-    pass
+    _rules = []
+    cursor.execute("SELECT Rule_1, Rule_2, Rule_3, Rule_4, Rule_5 FROM Manufacturers WHERE Code='?'", code)
+    for i in range(2, 7):
+        _rules.append(cursor.fetchone()[i])
+    return _rules
 
 
 def get_rules_from_name(name):
-    pass
+    _rules = []
+    cursor.execute("SELECT Rule_1, Rule_2, Rule_3, Rule_4, Rule_5 FROM Manufacturers WHERE Name='?'", name)
+    for i in range(2, 7):
+        _rules.append(cursor.fetchone()[i])
+    return _rules

238197d73fcf7a9f70fafa8ddf6a8665b50d3e98
Matthew Kuczak <matthewkuczak@gmail.com>
2018-09-20 15:27:22 -0500

    Added numbered_prompt function to main.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
 def main():
+    #numbered_prompt("Remove what?", refund.remove_from_items(), ["This", "That"])
     pass
+
+
+def numbered_prompt(heading, func, choices):
+    while True:
+        print(heading)
+        n = 1
+        for choice in choices:
+            print(str(n) + ': ' + choice)
+        try:
+            func(int(input("Input value: ")))
+            break
+        except ValueError:
+            print("ValueError: Try again.\n")
+        except IndexError:
+            print("IndexError: Try again.\n")
+        except AssertionError:
+            print("AssertionError: Try again.\n")
 
 
 main()
